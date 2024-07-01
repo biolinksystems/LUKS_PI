cd8d5d7ad9b974175aa52f07c0d25772c702fbb5
nikolai <144839823+nokiaangel@users.noreply.github.com>
2024-07-01 19:57:20 -0400

    Add files via upload

---
+++ initramfs-rebuild.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+ 
+
+if grep -q '\bsplash\b' /boot/cmdline.txt; then
+  sed -i 's/ \?splash \?/ /' /boot/cmdline.txt
+fi
+ 
+# Exit if not building kernel for this Raspberry Pi's hardware version.
+version="$1"
+current_version="$(uname -r)"
+case "${current_version}" in
+  *-v7+)
+    case "${version}" in
+      *-v7+) ;;
+      *) exit 0
+    esac
+  ;;
+  *+)
+    case "${version}" in
+      *-v7+) exit 0 ;;
+    esac
+  ;;
+esac
+
+[ -x /usr/sbin/mkinitramfs ] || exit 0
+[ -f /boot/initramfs.gz ] || exit 0
+lsinitramfs /boot/initramfs.gz |grep -q "/$version$" && exit 0  # Already in initramfs.
+ 
+mkinitramfs -o /boot/initramfs.gz "$version"
